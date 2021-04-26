dcfd333f7ff2dbabb4ccd0e6ae8e1e2f0ad9d95e
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2021-04-26 17:11:50 +0200

    updated with more tests

--- testg.ml
+++ testg.ml
@@ -3,24 +3,36 @@ open Graph_ext
 module G = UseGraph;;
 
 let g = G.empty ();;
-let (g, n1) = G.add g;;
 let (g, n2) = G.add g;;
 let (g, n3) = G.add g;;
-let g = G.set_edge g (n1, n2) 3;;
-let g = G.set_edge g (n3, n2) 1;;
-let g = G.set_edge g (G.source g, n1) 1;;
-let g = G.set_edge g (G.source g, n3) 1;;
-let g = G.set_edge g (n2, G.sink g) 3;;
+let (g, n4) = G.add g;;
+let (g, n5) = G.add g;;
+let g = G.set_edge g (G.source g, n2) 8;;
+let g = G.set_edge g (G.source g, n4) 4;;
+let g = G.set_edge g (n2, n3) 1;;
+let g = G.set_edge g (n4, n5) 7;;
+let g = G.set_edge g (n5, n3) 1;;
+let g = G.set_edge g (n3, G.sink g) 3;;
 
 G.print_graph g;;
+let (g, cut) = G.edmonds_karp g;;
+G.print_graph g;;
 
-let print_node (n : G.node) : unit =
-    Printf.printf "%s " (G.str_node n);;
+let g = G.empty ();;
+let (g, n2) = G.add g;;
+let (g, n3) = G.add g;;
+let (g, n4) = G.add g;;
+let (g, n5) = G.add g;;
+let (g, n6) = G.add g;;
+let g = G.set_edge g (G.source g, n2) 2;;
+let g = G.set_edge g (n2, n3) 3;;
+let g = G.set_edge g (n2, n4) 4;;
+let g = G.set_edge g (n3, n6) 1;;
+let g = G.set_edge g (n4, n5) 7;;
+let g = G.set_edge g (n5, n6) 1;;
+let g = G.set_edge g (n6, G.sink g) 10;;
 
-List.iter print_node (G.neighbours g (G.source g));;
-List.iter print_node (G.neighbours g (G.sink g));;
-List.iter print_node (G.neighbours g n1);;
-List.iter print_node (G.neighbours g n2);;
-List.iter print_node (G.neighbours g n3);;
+G.print_graph g;;
+let (g, cut) = G.edmonds_karp g;;
+G.print_graph g;;
 
-G.bfs g (fun v -> Printf.printf "%s\n" (G.str_node v));;
