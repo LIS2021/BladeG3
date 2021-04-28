0573c5c9fb3f6e0ccb4e13be9d5ff60e94502fda
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2021-04-28 23:14:48 +0200

    repo refactoring, def_use_gen.ml debugged, blade.ml added

--- def_use_gen.ml
+++ def_use_gen.ml
@@ -1,7 +1,9 @@
-open Ast
+(* open Ast *)
 open Graph
 
 (* TODO add function for different costs for branches *)
+
+module G = UseGraph
 
 (* Possible types of the nodes of the def-use graph *)
 type node_type =
@@ -16,36 +18,36 @@ module type DefUseGen = sig
   type generated
 
   (* create a new generator *)
-  val new_gen : generated
+  val new_gen : unit -> generated
 
   (* returns the graph generated *)
   val get_graph: generated -> graph
 
   (* creates, if not not already present, and returns a node*)
-  val new_node: t -> node_type -> node
+  val new_node: generated -> node_type -> node
 
-  val populate_graph: t -> cmd -> int -> unit
+  val populate_graph: generated -> cmd -> int -> unit
 
-  val populate_graph_exp: generated -> node_type -> int -> node
+  val populate_graph_exp: generated -> expr -> int -> node
 
-  val populate_graph_rhs: generated -> node_type -> int -> node
+  val populate_graph_rhs: generated -> rhs -> int -> node
 end
 
-module HashTableGen : DefUseGen
+module HashTableGen : DefUseGen = struct
 
-  type node = Graph.node
-  type graph = string Graph.graph 
+  type node = G.node
+  type graph = string G.graph 
   type generated = {
     hasht: (node_type, node) Hashtbl.t;
-    pairs: (node, identifier) list;
+    mutable pairs: (node * identifier) list;
     mutable g: graph;
   }
 
-  let new_gen = 
+  let new_gen _ = 
     {
-      hasht: Hashtbl.create 100;
-      pairs: [];
-      g: G.empty ();
+      hasht = Hashtbl.create 100;
+      pairs = [];
+      g = G.empty ();
     }
 
   let get_graph (gen: generated) : graph = gen.g
@@ -54,38 +56,39 @@ module HashTableGen : DefUseGen
     match Hashtbl.find_opt gen.hasht node_name with 
       | Some node -> node
       | None -> 
-        let (g, node) = G.add gen.g node_name in
+        let (g, node) = G.add gen.g in
         Hashtbl.add gen.hasht node_name node;
         gen.g <- g; node
 
   (* template cost function *)
-  let cost_f (x: int) : int = x
+  let cost_f (cost : int) : int = cost
  
   let rec populate_graph_exp (gen: generated) (e: expr) (cost: int) : node =
-    let source = G.source gen.g in
-    let sink = G.sink gen.g in
     match e with
       | Cst (v) ->
         new_node gen (NExpr e)
       | Var (id) ->
-        new_node gen (NVar e)
+        new_node gen (NVar id)
       | BinOp (e1, e2, op) ->
         let n_node = new_node gen (NExpr e) in
         let b1 = populate_graph_exp gen e1 cost in
         let b2 = populate_graph_exp gen e2 cost in
-        gen.g <- G.set_edge gen.g (b1, n_node) (cost_f cost); (* should these be -1 ?*)
-        gen.g <- G.set_edge gen.g (b2, n_node) (cost_f cost);
+        gen.g <- G.set_edge gen.g (b1, n_node) (-1); 
+        gen.g <- G.set_edge gen.g (b2, n_node) (-1);
         n_node
       | InlineIf (e1, e2, e3) ->
         let n_node = new_node gen (NExpr e) in
         let n1 = populate_graph_exp gen e1 cost in
         let n2 = populate_graph_exp gen e2 cost in
         let n3 = populate_graph_exp gen e3 cost in
-        gen.g <- G.set_edge gen.g (n1, n_node) (cost_f cost); (* should these be -1 ?*)
-        gen.g <- G.set_edge gen.g (n2, n_node) (cost_f cost);
-        gen.g <- G.set_edge gen.g (n3, n_node) (cost_f cost);
+        gen.g <- G.set_edge gen.g (n1, n_node) (-1);
+        gen.g <- G.set_edge gen.g (n2, n_node) (-1);
+        gen.g <- G.set_edge gen.g (n3, n_node) (-1);
         n_node
-  
+      | Length(a) -> G.sink gen.g (*TODO*)
+      | Base(a) -> G.sink gen.g (*TODO*)
+ 
+
   let rec populate_graph_rhs (gen: generated) (r: rhs) (cost: int) : node =
     let source = G.source gen.g in
     let sink = G.sink gen.g in
@@ -96,17 +99,17 @@ module HashTableGen : DefUseGen
       | PtrRead (e, l) ->
         let r_node = new_node gen (NRhs r) in
         let n = populate_graph_exp gen e cost in
-        gen.g <- G.set_edge gen.g (source, n) -1;
-        gen.g <- G.set_edge gen.g (n, sink) -1;
+        gen.g <- G.set_edge gen.g (source, n) (-1);
+        gen.g <- G.set_edge gen.g (n, sink) (-1);
         r_node
       | ArrayRead (a, e) ->
         let r_node = new_node gen (NRhs r) in
         let n = populate_graph_exp gen e cost in
-        gen.g <- G.set_edge gen.g (source, r_node) -1;
-        gen.g <- G.set_edge gen.g (n, sink) -1;
+        gen.g <- G.set_edge gen.g (source, r_node) (-1);
+        gen.g <- G.set_edge gen.g (n, sink) (-1);
         r_node
 
-  let rec populate_graph (gen: generated) (c: cmd) (cost: int): unit = 
+  let rec populate_graph (gen: generated) (c: cmd) (cost: int) : unit = 
     let sink = G.sink gen.g in
     match c with 
       | Fail -> ()
@@ -119,11 +122,11 @@ module HashTableGen : DefUseGen
       | PtrAssign (e1, e2, l) ->
         let ptr = populate_graph_exp gen e1 cost in
         let _ = populate_graph_exp gen e2 cost in
-        gen.g <- G.set_edge gen.g (ptr, sink) -1;
+        gen.g <- G.set_edge gen.g (ptr, sink) (-1);
       | ArrAssign (a, e1, e2) ->
         let _ = populate_graph_exp gen e1 cost in
         let index = populate_graph_exp gen e2 cost in
-        gen.g <- G.set_edge gen.g (index, sink) -1;
+        gen.g <- G.set_edge gen.g (index, sink) (-1)
       | Seq (c1, c2) -> 
         populate_graph gen c1 cost;
         populate_graph gen c2 cost
@@ -131,13 +134,14 @@ module HashTableGen : DefUseGen
         let _ = populate_graph gen c1 cost in
         let _ = populate_graph gen c2 cost in
         let cond_node = populate_graph_exp gen e cost in
-        gen.g <- G.set_edge gen.g (cond_node, sink) -1;
+        gen.g <- G.set_edge gen.g (cond_node, sink) (-1);
       | While (e, c) ->
         (* increase cost in cmds in a while block *)
         let n_cost = cost + 10 in
         let _ = populate_graph gen c n_cost in
         let cond_node = populate_graph_exp gen e cost in 
-        gen.g <- G.set_edge gen.g (cond_node, sink) -1;
+        gen.g <- G.set_edge gen.g (cond_node, sink) (-1);
       | Protect (id, p, rhs) ->
-        populate_graph_rhs gen rhs cost
-      end
+              let _ = populate_graph_rhs gen rhs cost in ()
+
+end
--- graph.ml
+++ graph.ml
@@ -15,6 +15,32 @@ module type Graph = sig
 
     val print_graph : 'a graph -> unit
 
+    val print_nodes : node list -> unit
+
+    val str_node : node -> string
+
+    val source : 'a graph -> node
+
+    val sink : 'a graph -> node
+
+    val neighbours : 'a graph -> node -> node list
+
+    val copy : 'a graph -> 'a graph
+
+    (*
+    val bfs : 'a graph -> (node -> unit) -> unit
+
+    val bfs_fp : 'a graph -> ('a graph * int array)
+
+    val max_flow : 'a graph -> int array -> int
+
+    val update_graph : 'a graph -> int array -> int -> 'a graph * int
+    *)
+
+    val edmonds_karp : 'a graph -> ('a graph * node list)
+
+    val filter_assoc : (node * 'b) list -> node list -> 'b list
+
 end
 
 module UseGraph : Graph = struct
@@ -26,33 +52,160 @@ module UseGraph : Graph = struct
     type edge = node * node
 
     let empty (() : unit) : 'a graph =
-        let src = [| 0 ; 0 |] in
-        let snk = [| 0 ; 0 |] in
-        { m = [| src ; snk |] };;
+        { m = [| [| 0 ; 0 |] ; [| 0 ; 0 |] |] }
 
     let add (g : 'a graph) : ('a graph * node) =
         let append_node ( a : int array ) = Array.append a [| 0 |] in
         let osz (m : int array array) : int = Array.length m in
         let append_all (m : int array array) = Array.map append_node m in
-        let add_array (m : int array array) = Array.append m [| (Array.make ((osz g.m) + 1) 0) |] in
-        ({ m = add_array (append_all g.m) }, (osz g.m) );;
+        let add_array (m : int array array) = 
+            Array.append m [| (Array.make ((osz g.m) + 1) 0) |] in
+        ({ m = add_array (append_all g.m) }, (osz g.m) )
 
     let set_edge (g : 'a graph) ((n1, n2) : edge) (c : int) : 'a graph =
-        g.m.(n1).(n2) <- c; g;;
+        g.m.(n1).(n2) <- c; g
 
     let capacity (g : 'a graph) ((n1, n2) : edge) : int =
-        g.m.(n1).(n2);;
+        g.m.(n1).(n2)
 
     let print_graph (g : 'a graph) : unit = 
         let helper i = (fun j n ->
             if n <> 0 then
-                Printf.printf "([%d;%d]: %d) " i j n) in
+                Printf.printf "([%d -> %d]: %d) " i j n) in
         let outer_iter (i : int) (a : int array) : unit = Array.iteri (helper i) a in 
-        Array.iteri outer_iter g.m;;
+        Array.iteri outer_iter g.m;
+        Printf.printf "\n"
 
+    let print_nodes (nds : node list) : unit =
+        Printf.printf "[ ";
+        List.iter (fun v -> Printf.printf "%d " v) nds;
+        Printf.printf "]\n"
+
+    let str_node (n : node) : string =
+        Printf.sprintf "%d" n
+
+    let source (g : 'a graph) : node = 0
+
+    let sink (g : 'a graph) : node = 1
+
+    let neighbours (g : 'a graph) (n : node) : node list =
+        let src = g.m.(n) in
+        let folder (it : node list) (v : node) : node list = 
+            if v >= 0 then v :: it else it in
+        let mapier (i : int) (n : int) : int = 
+            if n <> 0 then i else (-1) in
+        Array.fold_left folder [] (Array.mapi mapier src)
+
+    let copy (g : 'a graph) : 'a graph =
+        let maper (a : int array) : int array = Array.copy a in
+        let m' = Array.map maper g.m in
+        { m = m' }
+
+    let size (g : 'a graph) : int =
+        Array.length g.m
+
+    let bfs (g : 'a graph) (action : node -> unit) : unit =
+        let color = Array.make (size g) 0 in
+        let rec helper (stack : node list) (g : 'a graph) =
+            match stack with
+                | v :: vs -> 
+                        if color.(v) == 1 then helper vs g
+                        else
+                            let nb = neighbours g v in
+                            color.(v) <- 1;
+                            action v;
+                            helper (vs @ nb) g
+                | [] -> () in
+        helper [source g] g
+
+    let bfs_sp (g : 'a graph) : ('a graph * node array) =
+        let pred = Array.make (size g) (-1) in
+        let rec iter_nb (v : node) (nbs : node list) (stack : node list) : node list = 
+            match nbs with
+                | nb :: nbs' -> 
+                        if pred.(nb) = (-1) && nb <> 0 then
+                            (pred.(nb) <- v;
+                            iter_nb v nbs' (stack @ [nb]))
+                        else 
+                            iter_nb v nbs' stack
+                | [] -> stack in
+        let rec helper (stack : node list) (g : 'a graph) : ('a graph * node array) =
+            match stack with
+                | v :: vs -> 
+                        let stack' = iter_nb v (neighbours g v) vs in
+                        helper stack' g
+                | [] -> (g, pred) in
+        helper [source g] g
+
+
+    let (<?) (a : int) (b : int) : bool =
+        match a, b with
+            | -1, -1  -> false
+            | -1,  _  -> false
+            | _ , -1  -> true
+            | _ ,  _  -> (a < b)
+
+    let (-?) (a:int) (b:int) : int =
+        match a, b with
+            | -1, -1  -> -1 
+            | -1,  _  -> -1 
+            | _ , -1  -> -1 
+            | _ ,  _  -> (a - b)
+
+
+    let max_flow (g : 'a graph) (pred: node array) : int =
+        let rec folder (max : int) ((v1, v2) : (node * node)) : int =
+            if v1 = (-1) then max
+            else 
+                let cap = capacity g (v1, v2) in
+                let max' = if cap <? max then cap else max in
+                folder max' (pred.(v1), v1) in
+        let fe = (pred.(sink g), sink g) in
+        folder (capacity g fe) fe
+
+    let update_graph (g : 'a graph) (pred : node array) (max : int) : ('a graph * node) =
+        let rec updater (g : 'a graph) ((v1, v2) : (node * node)) (cut : node) : ('a graph * node) =
+            if v1 = (-1) then (g, cut) 
+            else 
+                let cap = capacity g (v1, v2) in
+                let g' = set_edge g (v1, v2) (cap -? max) in
+                let cut' = if (cap -? max) = 0 && cut = (-1) then v2 else cut in
+                updater g' (pred.(v1), v1) cut' in
+        updater g (pred.(sink g), sink g) (-1)
+
+    let edmonds_karp (g : 'a graph) : ('a graph * node list) =
+        let g' = copy g in
+        let rec add_absent (cuts : node list) (c : node) : (node list) =
+            match cuts with
+                | v :: vs -> 
+                        if v = c then v :: vs
+                        else v :: (add_absent vs c)
+                | [] -> [ c ] in
+        let rec helper (g : 'a graph) (cuts : node list) : ('a graph * node list) = 
+            let (g, pred) = bfs_sp g in
+            match pred.(sink g) with
+                | (-1) -> (g, cuts)
+                | _    -> let max = max_flow g pred in
+                          let (g, c) = update_graph g pred max in
+                          helper g (add_absent cuts c) in
+        helper g' []
+
+    let filter_assoc (ls : (node * 'b) list) (nodes : node list) : ('b list) =
+        let ls_sorter ((n1, _) : int * 'b) ((n2, _) : int * 'b) : int = n1 - n2 in
+        let nodes_sorter (n1 : int) (n2 : int) : int = n1 - n2 in
+        let ls' = List.sort ls_sorter ls in
+        let nodes' = List.sort nodes_sorter nodes in
+        let rec helper (ls : (node * 'b) list) (nodes : node list) (acc : 'b list) : ('b list) =
+            match ls, nodes with
+                | (v, l) :: ls', n :: nodes' -> 
+                        if n = v then helper ls' nodes' (l :: acc)
+                        else 
+                            if n > v then helper ls' nodes acc
+                            else helper ls nodes' acc
+                | [], _ -> acc
+                | _, [] -> acc in
+        helper ls' nodes' []
+                                                
 end
 
-
-
-        
 
