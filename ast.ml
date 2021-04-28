7c4f77b8f7ccaa11ddbfa95093cefc47b50619c8
Giuseppe Lomurno <lomurno.giuseppe97@gmail.com>
2021-04-28 02:32:25 +0200

    Parsing and reordering of files

--- ast.ml
+++ ast.ml
@@ -2,29 +2,59 @@ module StringMap = Map.Make(String)
 
 type identifier = string;;
 
-type op = string;;
+type op =
+  | Add
+  | Lte
+  | Lt
+  | BitAnd;;
+
+type label = unit;;
+
+type arr = {
+  base : int;
+  length : int;
+  label : label;
+};;
+
+let makeArr (b : int) (l : int) : arr =
+  {base = b; length = l; label = ()};;
+
+type value =
+  | CstI of int
+  | CstB of bool
+  | CstA of arr;;
+
+let makeInt (n : int) : value = CstI(n);;
+let makeBool (b : bool) : value = CstB(b);;
+let makeArray (a : arr) : value = CstA(a);;
 
 type expr =
-  | CstI of int
+  | Cst of value
   | Var of identifier
   | BinOp of expr * expr * op
   | InlineIf of expr * expr * expr
   | Length of expr
   | Base of expr;;
 
+let makeCst v = Cst(v);;
+let makeVar x = Var(x);;
+
 type rhs =
   | Expr of expr
-  | PtrRead of expr
-  | ArrayRead of identifier * expr;;
+  | PtrRead of expr * label
+  | ArrayRead of arr * expr;;
+
+let makeExpr e = Expr(e);;
+let makePtrRead e = PtrRead(e, ());;
 
 type protect = Slh | Fence | Auto;;
 
 type cmd =
-  | Skip 
+  | Skip
   | Fail
   | VarAssign of identifier * rhs
-  | PtrAssign of expr * expr
-  | ArrAssign of identifier * expr * expr
+  | PtrAssign of expr * expr * label
+  | ArrAssign of arr * expr * expr
   | Seq of cmd * cmd
   | If of expr * cmd * cmd
   | While of expr * cmd
@@ -39,40 +69,25 @@ type directive =
   | Exec of int
   | Retire
 
+type guard_fail_id = int;;
+
 (** 		OBSERVATIONS 		**)
 type observation =
   | None
   | Read of int * int list
   | Write of int * int list
-  | Fail of int
+  | OFail of guard_fail_id
   | Rollback of int
 
 (**		INSTRUCTION SET		**)
 type instruction =
   | Nop
-  | Assign of identifier * expr
-  | Load of identifier * expr 			(* 	id := load(e) 		*)
-  | Store of expr * expr
-  | IProtect of identifier * protect * expr 	(* 	id := protect(e) 	*)
-  | Guard of expr * prediction * cmd list * int
-  | Fail of int ;;
-
-(**		CONFIGURATIONS 		**)
-type configuration = {
-	mutable is : instruction list ; 
-	mutable cs : cmd list ; 
-	mutable mu : int array ; 
-	mutable rho : int StringMap.t ;
-};;
-
-type decl_type =
-  | TypI
-  | TypA of int * int
-  | TypP;;
-
-let rec eval (conf: configuration) (decls : decl_type StringMap.t) (attacker : configuration -> directive) (obs : observation list) (count : int) : configuration * observation list * int =
-	failwith "not implemented";;
-
-let rec blade (command : cmd) : cmd =
-	failwith "not implemented";;
-
+  | AssignE of identifier * expr
+  | AssignV of identifier * value
+  | Load of identifier * label * expr 			(* 	id := load(e) 		*)
+  | StoreE of expr * expr
+  | StoreV of int * int
+  | IProtectE of identifier * protect * expr 	(* 	id := protect(e) 	*)
+  | IProtectV of identifier * value 	(* 	id := protect(v) 	*)
+  | Guard of expr * prediction * cmd list * guard_fail_id
+  | IFail of guard_fail_id ;;
